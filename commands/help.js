be91446cb35a4053a72e184457ac8c9e53a41e54
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-24 19:09:07 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,8 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
   let botEmbed = new Discord.RichEmbed()
   .setColor('#fff200')
-  .setDescription("*coming soon...*");
+  .setTitle("Bot Commands:")
+  .setDescription("`,tics` - shows number of ticks you have.\n`,punish` - shows how users get punished.");
   message.channel.send(botEmbed)
 }
 	      
