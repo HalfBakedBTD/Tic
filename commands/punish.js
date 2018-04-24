65f0fe4bd12bcc9357cc06f7a749ae683da8d6ec
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-24 19:26:34 -0400

    Update punish.js

--- commands/punish.js
+++ commands/punish.js
@@ -3,8 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
   let botEmbed = new Discord.RichEmbed()
   .setColor('#fff200')
-  .setTitle("Punishment System:")
-  .setDescription("\t-5 tics: 15 minute mute.\n\t-10 tics: 30 minute mute.\n\t-15 tics: 1 hour mute.\n\t-20 tics: 2 hour mute.\n\t-25 tics: 4 hour mute.\n\t-32 tics: 8 hour mute.\n\t-40 tics: mute until bot restart. (Up to 24h)\n\t-50 tics: perm mute.");
+  .setDescription("**__Punishment System:__**\n\t-5 tics: 15 minute mute.\n\t-10 tics: 30 minute mute.\n\t-15 tics: 1 hour mute.\n\t-20 tics: 2 hour mute.\n\t-25 tics: 4 hour mute.\n\t-32 tics: 8 hour mute.\n\t-40 tics: mute until bot restart. (Up to 24h)\n\t-50 tics: perm mute.");
   message.channel.send(botEmbed)
 }
 	      
